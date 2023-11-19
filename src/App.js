7f6353f5c10ad8304c8f100b823cd406254f5b4b
Sushmita <sushmitakhadka097@gmail.com>
2023-11-19 07:45:56 +0545

    FIrst Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import head from './components/Header'
+
+function App() {
+  return (
+    <div className='App'>
+      <head />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const head = () => {
+    return (
+        <div>
+            header
+        </div>
+    )
+}
+
+export default head
